8c61239daf608187191499b7a8848d228aab4d95
Michael Leisch <92372034+Michiii11@users.noreply.github.com>
2022-03-21 08:14:05 +0100

    some changes

--- script.js
+++ script.js
@@ -1,35 +1,82 @@
 // Function to Switch between Dark and White Mode
-mode = "WHITE"
-function changeMode(){
-    if(mode == "BLACK"){
-        console.log("BLACK");
-        mode = "WHITE"
-        document.getElementById("switch").innerHTML = `<i class="fa-solid fa-toggle-on">`
-        document.documentElement.style.setProperty('--background', "rgb(29, 29, 29)");
-        document.documentElement.style.setProperty('--color', "rgb(240,240,240)"); 
-        document.documentElement.style.setProperty('--darkShadow', "20px 20px 60px #191919,-20px -20px 60px #212121");
-        document.documentElement.style.setProperty('--dark1', "#1a1a1a");
-        document.documentElement.style.setProperty('--dark2', "#1f1f1f");
-    }
-    else if(mode == "WHITE"){
-        mode = "BLACK"
-        document.getElementById("switch").innerHTML = `<i class="fa-solid fa-toggle-off">`
-        document.documentElement.style.setProperty('--background', "rgb(240,240,240)");
-        document.documentElement.style.setProperty('--color', "rgb(29, 29, 29)");
-        document.documentElement.style.setProperty('--darkShadow', "20px 20px 60px #cccccc,-20px -20px 60px #ffffff");
-        document.documentElement.style.setProperty('--dark1', "#d8d8d8");
-        document.documentElement.style.setProperty('--dark2', "#ffffff");
-    }
+function changeMode() {
+    //Swap Main Colors
+    let temp = getComputedStyle(document.documentElement).getPropertyValue('--color1');
+    document.documentElement.style.setProperty('--color1', getComputedStyle(document.documentElement).getPropertyValue('--color2'));
+    document.documentElement.style.setProperty('--color2', temp);
+
+    //Swap Shadows
+    temp = getComputedStyle(document.documentElement).getPropertyValue('--lightShadow');
+    document.documentElement.style.setProperty('--lightShadow', getComputedStyle(document.documentElement).getPropertyValue('--darkShadow'));
+    document.documentElement.style.setProperty('--darkShadow', temp);
+
+    //Swap Box Colors 1
+    temp = getComputedStyle(document.documentElement).getPropertyValue('--darkBox1');
+    document.documentElement.style.setProperty('--darkBox1', getComputedStyle(document.documentElement).getPropertyValue('--lightBox1'));
+    document.documentElement.style.setProperty('--lightBox1', temp);
+
+    //Swap Box Colors 2
+    temp = getComputedStyle(document.documentElement).getPropertyValue('--darkBox2');
+    document.documentElement.style.setProperty('--darkBox2', getComputedStyle(document.documentElement).getPropertyValue('--lightBox2'));
+    document.documentElement.style.setProperty('--lightBox2', temp)
 }
 
 loadDashboard();
 
-function loadDashboard(){
-    document.getElementById("content").innerHTML = `<form><input id="input" type="text" onchange="makeLektion(this)" placeholder="/baum/haus/..."></form>`
+function loadDashboard() {
+    let nav = `<a onclick="loadDashboard()"><div><h3>Easy Learning</h3></div></a>
+                <a onclick="loadDashboard()"><div><i id="active" class="fa-solid fa-house"></i></div></a>
+                <a onclick="addLektion()"><div><i class="fa-solid fa-plus"></i></div></a>
+                <a onclick="searchLektion()"><div><i class="fa-solid fa-magnifying-glass"></i></div></a>
+                <a onclick="changeMode()"><div id="switch"><i class="fa-solid fa-toggle-on"></i></div></a>`
+    document.getElementById('nav').innerHTML = nav;
+
+    temp = "";
+    temp += `<div id="previewField">`
+    temp += `<div class="preview" onclick="loadLektion()"><h1>Titel</h1><p>10 Begriffe</p></div>`
+    temp += `<div class="preview" onclick="loadLektion()"><h1>Titel</h1><p>10 Begriffe</p></div>`
+    temp += `<div class="preview" onclick="loadLektion()"><h1>Titel</h1><p>10 Begriffe</p></div>`
+    temp += `<div class="preview" onclick="loadLektion()"><h1>Titel</h1><p>10 Begriffe</p></div>`
+    temp += `<div class="preview" onclick="loadLektion()"><h1>Titel</h1><p>10 Begriffe</p></div>`
+    temp += `<div class="preview" onclick="loadLektion()"><h1>Titel</h1><p>10 Begriffe</p></div>`
+    temp += `<div class="preview" onclick="loadLektion()"><h1>Titel</h1><p>10 Begriffe</p></div>`
+    temp += `</div>`
+
+    document.getElementById('content').innerHTML = temp;
 }
-function addLektion(){
+
+content = "";
+
+function addLektion() {
+    let nav = `<a onclick="loadDashboard()"><div><h3>Easy Learning</h3></div></a>
+                <a onclick="loadDashboard()"><div><i class="fa-solid fa-house"></i></div></a>
+                <a onclick="addLektion()"><div><i id="active" class="fa-solid fa-plus"></i></div></a>
+                <a onclick="searchLektion()"><div><i class="fa-solid fa-magnifying-glass"></i></div></a>
+                <a onclick="changeMode()"><div id="switch"><i class="fa-solid fa-toggle-on"></i></div></a>`
+    document.getElementById('nav').innerHTML = nav;
+
+    content = `<div id="addLektion"><h1>Neue Lektion erstellen</h1>`
+    content += `<form><br><input class="input" id="lektionName" type="text" placeholder="Gib einen Titel ein wie 'Englisch - Kapitel 1: Welcome Back'"></form>`
+    content += `<div id='autoGenerate' onclick="loadAutoGenerate()">Auto Generate Table</div>`
+
+    content += "<table><tr><th>Begriff</th><th id='definition' onclick='autoEnglisch()'>Defintion</th></tr>"
+    content += `<tr><td class="begriff" onClick="javascript:onSelect(this)"><input value="" onclick="this.select()"></td><td class="definition"><input value="" onclick="this.select()"></td></tr>`
+    content += `<tr><td colspan="2" id="lastRow" onclick="addRow()"><i class="fa-solid fa-plus"></td></tr>`
+    content += "</table>";
+    content += "<div id='confirm' onclick='saveLektion()'>Save</div>"
+
+    document.getElementById('content').innerHTML = content;
+}
+
+function searchLektion() {
 
 }
-function searchLektion(){
 
+function addRow() {
+    content = content.substr(0, content.length - 150);
+    content += `<tr><td onClick="javascript:onSelect(this)"><input value="" onclick="this.select()"></td><td><input value="" onclick="this.select()"></td></tr>`
+    content += `<tr><td colspan="2" id="lastRow" onclick="addRow()"><i class="fa-solid fa-plus"></td></tr>`
+    content += "</table>";
+    content += "<div id='confirm' onclick='saveLektion()'>Save</div>"
+    document.getElementById("content").innerHTML = content;
 }
--- tool.js
+++ tool.js
@@ -1,19 +1,127 @@
+let content;
+let lektion;
+let lektionName = "";
+let lektionCount = 0;
+
+if(!localStorage["lektionSaves"]){
+    lektion = ["", "", "", "", "", "", "", "", "", ""]
+    console.log(lektion);
+    for(let i = 0; i < lektion.length; i++){
+        lektion[i] = new Array();
+    }
+} else{
+    lektion = localStorage["lektionSaves"];
+}
+
 function makeLektion(elem) {
     document.getElementById("content").innerHTML = ``
     elem = elem.value;
     char = elem.charAt(0);
     elem = elem.split(char)
+    lektionName = document.getElementById('lektionName');
 
-
-    let content = "<table><tr><th>Deutsch</th><th>Englisch</th></tr>"
+    content = `<div id="addLektion"><h1>Neue Lektion erstellen</h1>`
+    content += `<form><br><input class="input" id="lektionName" type="text" placeholder="Lektion 1">
+               <br><input class="input" type="text" onchange="makeLektion(this)" placeholder="/baum/haus/..."></form>`
+    content += "<table><tr><th>Begriff</th><th id='englisch' onclick='autoEnglisch()'>Definition</th></tr>"
     for (let i = 1; i < elem.length; i++) {
-        content += `<tr><td onClick="javascript:onSelect(this)"><input value="${elem[i]}" onclick="this.select()"></td><td><input value="" onclick="this.select()"></td></tr>`
+        content += `<tr><td class="begriff" onClick="javascript:onSelect(this)"><input value="${elem[i]}" onclick="this.select()"></td><td class="definition"><input value="" onclick="this.select()"></td></tr>`
     }
+    content += `<tr><td colspan="2" id="lastRow" onclick="addRow()"><i class="fa-solid fa-plus"></td></tr>`
     content += "</table>";
-    content += "<div id='confirm'>Save</div>"
+    content += "<div id='confirm' onclick='saveLektion()'>Save</div>"
+
+    for(let i = 0; i < document.getElementsByClassName("begriff").length; i++){
+        
+    }
+    lektion[lektionCount] = elem;
     document.getElementById("content").innerHTML = content;
 }
 
-function saveLektion(elem) {
-    
+function autoEnglisch() {
+    let lektionEnglisch;
+
+    fetch("https://libretranslate.de/translate", {
+            method: "POST",
+            body: JSON.stringify({
+                q: "",
+                source: "de",
+                target: "en",
+                format: "text"
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        // PARSE
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        // LOGGER
+        .then((erg) => {
+            console.log(erg);
+            return erg;
+        })
+        .catch((error) => {
+            console.log('Error: ', error);
+        })
+
+}
+
+function addRow() {
+    content = content.substr(0, content.length - 150);
+    content += `<tr><td onClick="javascript:onSelect(this)"><input value="" onclick="this.select()"></td><td><input value="" onclick="this.select()"></td></tr>`
+    content += `<tr><td colspan="2" id="lastRow" onclick="addRow()"><i class="fa-solid fa-plus"></td></tr>`
+    content += "</table>";
+    content += "<div id='confirm' onclick='saveLektion()'>Save</div>"
+    document.getElementById("content").innerHTML = content;
+}
+
+function saveLektion() {
+    localStorage["lektionSaves"] = lektion;
+    loadLektion(lektion[lektionCount])
+}
+function loadLektion(thisLektion) {
+    let nav = `<a onclick="loadDashboard()"><div><h3>Easy Learning</h3></div></a>
+                <a onclick="loadDashboard()"><div><i class="fa-solid fa-house"></i></div></a>
+                <a onclick="addLektion()"><div><i class="fa-solid fa-plus"></i></div></a>
+                <a onclick="searchLektion()"><div><i class="fa-solid fa-magnifying-glass"></i></div></a>
+                <a onclick="changeMode()"><div id="switch"><i class="fa-solid fa-toggle-on"></i></div></a>`
+    document.getElementById('nav').innerHTML = nav;
+
+    let temp = ""
+    temp += ""
+    document.getElementById('content').innerHTML = temp;
+}
+
+function loadAutoGenerate(){
+    let temp = `<div id='loadAutoGenerate'>`
+    temp += `<textarea id="text"></textarea>`
+    temp += `<div>
+    <a>Zwischen Begriffen und Definition</a>  
+    <div class="toggle-buttons">
+        <input type="radio" id="a1" name="group-a"/>
+        <label for="b1">Tab</label>
+        <input type="radio" id="a2" name="group-a"/>
+        <label for="b2">Komma</label>
+        <input type="radio" id="a3" name="group-a" />
+        <label for="b3"><input name="text" placeholder="-"></label>
+    </div>`
+
+    temp += `
+    <a>Zwischen Karten</a>  
+    <div class="toggle-buttons-2">
+        <input type="radio" id="b1" name="group-b"/>
+        <label for="b1">Tab</label>
+        <input type="radio" id="b2" name="group-b"/>
+        <label for="b2">Komma</label>
+        <input type="radio" id="b3" name="group-b" />
+        <label for="b3"><input name="text" placeholder='ENTER'></label>
+    </div>`
+
+    temp += `<div id='confirm' onclick='saveLektion()'>Save</div></div>`
+    temp += `</div>`
+
+    document.getElementById('content').innerHTML = temp;
 }
